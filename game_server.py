c0ba010c2242843203fa75de982305a3aa229675
mjlomeli <mjlomeli@uci.edu>
2022-01-29 19:15:03 -0800

    saving copied and pasted from my desktop version

--- game_server.py
+++ game_server.py
@@ -64,61 +64,15 @@ class GameServer:
         path = ['services', self.service_id, 'gameservers', 'games', 'arkse', 'gen_cluster_id']
         return GameServer.CLIENT.get(path=path)['data']['clusterid']
 
-    def details(self):
-        return GameServer.CLIENT.get(path=['services', self.service_id, 'gameservers'])['data']
-
     def start(self, game):
         path = ['services', self.service_id, 'gameservers', 'games', 'start']
         params = {'game': game}
-        return GameServer.CLIENT.post(path=path, params=params)
-
-    def restart(self, restart_message=None, log_message=None):
-        path = ['services', self.service_id, 'gameservers', 'restart']
-        params = {"restart_message": restart_message, "message": log_message}
-        return GameServer.CLIENT.post(path=path, params=params)
-
-    def stop(self, message=None, stop_message=None):
-        path = ['services', self.service_id, 'gameservers', 'stop']
-        params = {'message': message, 'stop_message': stop_message}
-        return GameServer.CLIENT.post(path=path, params=params)
-
-    def list_backups(self):
-        path = ['services', self.service_id, 'gameservers', 'backups']
-        return GameServer.CLIENT.get(path=path)['data']
-
-    def command(self, command):
-        path = ['services', self.service_id, 'gameservers', 'app_server', 'command']
-        params = {'command': command}
-        return GameServer.CLIENT.post(path=path, params=params)
-
-    def ping(self):
-        path = ['services', self.service_id, 'gameservers', 'app_server']
-        return GameServer.CLIENT.get(path=path)
-
-    def restore_database(self, db_name, timestamp):
-        path = ['services', self.service_id, 'gameservers', 'backups', 'database']
-        params = {'database': db_name, 'timestamp': timestamp}
         return GameServer.CLIENT.post(path=path, params=params)
 
     def backup_restore(self, folder, backup_id):
         path = ['services', self.service_id, 'gameservers', 'backups', 'gameserver']
         params = {'folder': folder, 'backup': backup_id}
         return GameServer.CLIENT.post(path=path, params=params)
-
-    def duplicate_file(self, source_path, target_path, target_name):
-        path = ['services', self.service_id, 'gameservers', 'file_server', 'copy']
-        params = {'source_path': source_path, 'target_path': target_path, 'target_name': target_name}
-        return GameServer.CLIENT.post(path=path, params=params)
-
-    def create_directory(self, dir_path, name):
-        path = ['services', self.service_id, 'gameservers', 'file_server', 'mkdir']
-        params = {'path': dir_path, 'name': name}
-        return GameServer.CLIENT.post(path=path, params=params)
-
-    def delete_file(self, file_path):
-        path = ['services', self.service_id, 'gameservers', 'file_server', 'delete']
-        params = {'path': file_path}
-        return GameServer.CLIENT.delete(path=path, params=params)
 
     def download_file(self, file_path):
         path = ['services', self.service_id, 'gameservers', 'file_server', 'download']
@@ -129,20 +83,18 @@ class GameServer:
         params = {'path': file_path}
         return GameServer.CLIENT.get(path=path, params=params)
 
-    def list_files(self, dir_path=None, search=None):
-        path = ['services', self.service_id, 'gameservers', 'file_server', 'list']
-        params = {'dir': dir_path, 'search': search}
-        return GameServer.CLIENT.get(path=path, params=params)
-
     def rename_file(self, file_path, target_path, target_name):
         path = ['services', self.service_id, 'gameservers', 'file_server', 'move']
         params = {'source_path': file_path, 'target_path': target_path, 'target_filename': target_name}
         return GameServer.CLIENT.post(path=path, params=params)
 
-    def upload_file(self, target_dir, target_name):
-        path = ['services', self.service_id, 'gameservers', 'file_server', 'upload']
-        params = {'path': target_dir, 'file': target_name}
-        return GameServer.CLIENT.post(path=path, params=params)
+    # def rename_file(self, file_path, new_name):
+    #     path = ['services', self.service_id, 'gameservers', 'file_server', 'move']
+    #     target_path = '/'.join(file_path.split("/")[:-1])
+    #     params = {'source_path': file_path, 'target_path': target_path, 'target_filename': new_name}
+    #     return self._client.post(path=path, params=params)
+
+
 
     def donation_history(self, page=0):
         path = ['services', self.service_id, 'gameservers', 'boost', 'history']
@@ -177,6 +129,201 @@ class GameServer:
         path = ['services', self.service_id, 'gameservers', 'adminlist']
         params = {'identifier': gamertag}
         return GameServer.CLIENT.post(path=path, params=params)
+
+    def details(self):
+        path = ['services', self.service_id, 'gameservers']
+        return GameServer.CLIENT.get(path=path)
+
+    def restart(self, restart_message: str = None, log_message: str = None):
+        path = ['services', self.service_id, 'gameservers', 'restart']
+        params = {"restart_message": restart_message, "message": log_message}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def stop(self, message=None, stop_message=None):
+        path = ['services', self.service_id, 'gameservers', 'stop']
+        params = {'message': message, 'stop_message': stop_message}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def list_backups(self):
+        path = ['services', self.service_id, 'gameservers', 'backups']
+        return GameServer.CLIENT.get(path=path)
+
+    def command(self, command):
+        path = ['services', self.service_id, 'gameservers', 'app_server', 'command']
+        params = {'command': command}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def ping(self):
+        path = ['services', self.service_id, 'gameservers', 'app_server']
+        return GameServer.CLIENT.get(path=path)
+
+    def restore_database(self, database: str, timestamp: str):
+        path = ['services', self.service_id, 'gameservers', 'backups', 'database']
+        params = {'database': database, 'timestamp': timestamp}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+
+    def ftp_change_password(self, password: str):
+        path = ['services', self.service_id, 'gameservers', 'ftp', 'password']
+        params = {'password': password}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def bookmarks(self):
+        path = ['services', self.service_id, 'gameservers', 'file_server', 'bookmarks']
+        return GameServer.CLIENT.get(path=path)
+
+    def copy_file(self, source_path: str, target_path: str, target_name: str):
+        path = ['services', self.service_id, 'gameservers', 'file_server', 'copy']
+        params = {'source_path': source_path, 'target_path': target_path, 'target_name': target_name}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def create_directory(self, dir_path, name):
+        path = ['services', self.service_id, 'gameservers', 'file_server', 'mkdir']
+        params = {'path': dir_path, 'name': name}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def delete_file(self, file_path):
+        path = ['services', self.service_id, 'gameservers', 'file_server', 'delete']
+        params = {'path': file_path}
+        return GameServer.CLIENT.delete(path=path, params=params)
+
+    def list_files(self, dir_path=None, search: str = None):
+        path = ['services', self.service_id, 'gameservers', 'file_server', 'list']
+        params = {'dir': dir_path, 'search': search}
+        return GameServer.CLIENT.get(path=path, params=params)
+
+    def move_file(self, source_path: str, target_path: str):
+        path = ['services', self.service_id, 'gameservers', 'file_server', 'move']
+        name = source_path.split('/')[-1]
+        params = {'source_path': source_path, 'target_path': target_path, 'target_file_name': name}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def seek_file(self, file_path: str, offset: int, length: int, mode: str = None):
+        path = ['services', self.service_id, 'gameservers', 'file_server', 'seek']
+        params = {'file': file_path, 'offset': offset, 'length': length, 'mode': mode}
+        return GameServer.CLIENT.get(path=path, params=params)
+
+    def file_size(self, file_path):
+        path = ['services', self.service_id, 'gameservers', 'file_server', 'size']
+        params = {'file': file_path}
+        return GameServer.CLIENT.get(path=path, params=params)
+
+    def duplicate_file(self, source_path, target_path, target_name):
+        path = ['services', self.service_id, 'gameservers', 'file_server', 'copy']
+        params = {'source_path': source_path, 'target_path': target_path, 'target_name': target_name}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def files_stat(self, files_paths: list):
+        path = ['services', self.service_id, 'gameservers', 'file_server', 'stat']
+        params = {'files': files_paths}
+        return GameServer.CLIENT.get(path=path, params=params)
+
+    def upload_file(self, target_dir: str, file_name: str):
+        path = ['services', self.service_id, 'gameservers', 'file_server', 'upload']
+        params = {'path': target_dir, 'file': file_name}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def list_all_games(self):
+        path = ['gameserver', 'games']
+        return GameServer.CLIENT.get(path=path)
+
+    def install_game(self, game: str, modpack: str = None):
+        path = ['services', self.service_id, 'gameservers', 'games', 'install']
+        params = {'game': game, 'modpack': modpack}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def list_games(self):
+        path = ['services', self.service_id, 'gameservers', 'games']
+        return GameServer.CLIENT.get(path=path)
+
+    def start_game(self, game: str):
+        path = ['services', self.service_id, 'gameservers', 'games', 'start']
+        params = {'game': game}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def uninstall_game(self, game: str):
+        path = ['services', self.service_id, 'gameservers', 'games', 'uninstall']
+        params = {'game': game}
+        return GameServer.CLIENT.delete(path=path, params=params)
+
+    def change_mysql_password(self, password: str):
+        path = ['services', self.service_id, 'gameservers', 'mysql', 'password']
+        params = {'password': password}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def reset_mysql_database(self, password: str):
+        path = ['services', self.service_id, 'gameservers', 'mysql', 'reset']
+        params = {'password': password}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def install_package(self, package: str, version: str = None):
+        path = ['services', self.service_id, 'gameservers', 'packages']
+        params = {'package': package, 'version': version}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def list_packages(self):
+        path = ['services', self.service_id, 'gameservers', 'packages']
+        return GameServer.CLIENT.post(path=path)
+
+    def reinstall_package(self, package: str, version: str = None):
+        path = ['services', self.service_id, 'gameservers', 'packages', 'reinstall']
+        params = {'package': package, 'version': version}
+        return GameServer.CLIENT.put(path=path, params=params)
+
+    def uninstall_package(self, package: str):
+        path = ['services', self.service_id, 'gameservers', 'packages', 'uninstall']
+        params = {'package': package}
+        return GameServer.CLIENT.delete(path=path, params=params)
+
+    def resource_usage(self, hours: int = None):
+        path = ['services', self.service_id, 'gameservers', 'stats']
+        params = {'hours': hours}
+        return GameServer.CLIENT.get(path=path, params=params)
+
+    def create_settings_sets(self, name: str = None):
+        path = ['services', self.service_id, 'gameservers', 'settings', 'sets']
+        params = {'name': name}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def delete_settings_sets(self, set_id: int):
+        path = ['services', self.service_id, 'gameservers', 'settings', 'sets', set_id]
+        return GameServer.CLIENT.delete(path=path)
+
+    def list_settings_sets(self):
+        path = ['services', self.service_id, 'gameservers', 'settings', 'sets']
+        return GameServer.CLIENT.get(path=path)
+
+    def restore_settings_sets(self, set_id: int):
+        path = ['services', self.service_id, 'gameservers', 'settings', 'sets', set_id, 'restore']
+        return GameServer.CLIENT.post(path=path)
+
+    def default_settings(self):
+        path = ['services', self.service_id, 'gameservers', 'settings', 'defaults']
+        return GameServer.CLIENT.get(path=path)
+
+    def reset_settings(self):
+        path = ['services', self.service_id, 'gameservers', 'settings']
+        return GameServer.CLIENT.delete(path=path)
+
+    def update_settings(self, category: str, key: str, value: str):
+        path = ['services', self.service_id, 'gameservers', 'settings']
+        params = {'category': category, 'key': key, 'value': value}
+        return GameServer.CLIENT.post(path=path, params=params)
+
+    def boost_history(self, page: int = 0):
+        path = ['services', self.service_id, 'gameservers', 'boost', 'history']
+        params = {'page': page}
+        return GameServer.CLIENT.get(path=path, params=params)
+
+    def boost_settings(self):
+        path = ['services', self.service_id, 'gameservers', 'boost']
+        return GameServer.CLIENT.get(path=path)
+
+    def update_boost_settings(self, enable: bool = True, message: str = None, welcome_message: str = None):
+        path = ['services', self.service_id, 'gameservers', 'boost']
+        params = {'enable': enable, 'message': message, 'welcome_message': welcome_message}
+        return GameServer.CLIENT.put(path=path, params=params)
+
 
     def __repr__(self):
         return f"<GameServer(service_id={self.service_id}, status='{self.status}', query={self.query})>"
