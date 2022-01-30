2a59ee22d0f1f151b394a39f99edf0c0c43c7666
mjlomeli <mjlomeli@uci.edu>
2022-01-30 08:36:24 -0800

    finished the nitrado api

--- nitrado_api.py
+++ nitrado_api.py
@@ -1,29 +1,56 @@
+import client
 from client import Client
-
+from game_server import GameServer
+from service import Service
+import requests
+import os
 
 class NitradoAPI:
     NITRADO_API_URL = "https://api.nitrado.net/"
+    CLIENT = Client.CLIENT
 
     @classmethod
-    def __default_access(cls):
-        import os
-        return os.environ.get("NITRADO_KEY")
+    def initialize_client(cls, url, key):
+        client = Client(url, key)
+        Client.CLIENT = client
+        GameServer.CLIENT = client
+        Service.CLIENT = client
+        NitradoAPI.CLIENT = client
+
+    @classmethod
+    def unofficial_server_list(cls):
+        url = "http://arkdedicated.com/xbox/cache/unofficialserverlist.json"
+        req = requests.get(url)
+        return req.json()
+
+    @classmethod
+    def official_server_list(cls):
+        url = "http://arkdedicated.com/xbox/cache/officialserverlist.json"
+        req = requests.get(url)
+        return req.json()
+
+    @classmethod
+    def banned_list(cls):
+        url = "http://arkdedicated.com/xboxbanlist.txt"
+        req = requests.get(url)
+        return req.text.split('\n')
 
     def __init__(self, key=None, url=None):
-        key = key or NitradoAPI.__default_access()
+        key = key or os.environ.get("NITRADO_KEY")
         url = url or NitradoAPI.NITRADO_API_URL
         assert key and url, f"params in NitradoApi({key}, {url}) must be provided"
-        self.__client = Client(url, key)
-        self.__servers = []
+        NitradoAPI.initialize_client(url, key)
+        self.services = Service.all()
+        self.games = GameServer.all()
 
     def health_check(self):
-        return self.__client.get('ping')
+        return NitradoAPI.CLIENT.get('ping')
 
     def maintenance_status(self):
-        return self.__client.get('maintenance')
+        return NitradoAPI.CLIENT.get('maintenance')
 
     def current_api_version(self):
-        return self.__client.get('version')
+        return NitradoAPI.CLIENT.get('version')
 
 
 if __name__ == '__main__':
@@ -37,5 +64,9 @@ if __name__ == '__main__':
     print_json(api.maintenance_status())
     print_json(api.current_api_version())
 
-    # api.restart(9409179, restart_message="lets restart this server!")
+    for service in api.services:
+        print(service)
+
+    for game in api.games:
+        print(game)
 
