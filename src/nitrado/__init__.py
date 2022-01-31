68e3f375c9f88de7f883ad7d2642114960da6507
mjlomeli <mjlomeli@uci.edu>
2022-01-31 09:06:30 -0800

    added ocr to the namespace

--- src/nitrado/__init__.py
+++ src/nitrado/__init__.py
@@ -2,6 +2,7 @@ from nitrado.nitrado_api import NitradoAPI
 from nitrado.service import Service
 from nitrado.game_server import GameServer
 from nitrado.client import Client
+from nitrado.ocr import ocr
 
 
 def initialize_client(url=None, key=None):
