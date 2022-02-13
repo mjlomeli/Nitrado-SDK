23e84def0dd69e4c5b5b6670c3abcfff5b961232
mjlomeli <mjlomeli@uci.edu>
2022-02-12 18:52:59 -0800

    testing

--- src/nitrado/__init__.py
+++ src/nitrado/__init__.py
@@ -4,7 +4,7 @@ from nitrado.game_server import GameServer
 from nitrado.client import Client
 
 
-__all__ = ['NitradoAPI', 'Service', 'GameServer', 'Client']
+__all__ = ['NitradoAPI', 'Service', 'GameServer', 'Client', 'initialize_client']
 
 
 def initialize_client(key=None, url=None):
--- tests/test_client.py
+++ tests/test_client.py
@@ -10,8 +10,6 @@ def get_client():
 
 
 def test_ping():
-    env = os.environ.keys()
-    assert os.getenv('NITRADO_KEY') is not None, f"{env}"
     client = get_client()
     ping_json = client.get('ping')
     assert ping_json is not None
@@ -19,29 +17,29 @@ def test_ping():
     assert ping_json['status'] == 'success'
 
 
-# def test_maintenance():
-#     client = get_client()
-#     maint = client.get('maintenance')
-#     assert maint is not None
-#     assert 'status' in maint
-#     assert maint['status'] == 'success'
-#
-#
-# def test_version():
-#     client = get_client()
-#     version = client.get('version')
-#     assert version is not None
-#     assert 'status' in version
-#     assert version['status'] == 'success'
-#
-#
-# def test():
-#     test_ping()
-#     test_version()
-#     test_maintenance()
-#
-#
-# if __name__ == "__main__":
-#     test()
-#     print("passing")
-#
+def test_maintenance():
+    client = get_client()
+    maint = client.get('maintenance')
+    assert maint is not None
+    assert 'status' in maint
+    assert maint['status'] == 'success'
+
+
+def test_version():
+    client = get_client()
+    version = client.get('version')
+    assert version is not None
+    assert 'status' in version
+    assert version['status'] == 'success'
+
+
+def test():
+    test_ping()
+    test_version()
+    test_maintenance()
+
+
+if __name__ == "__main__":
+    test()
+    print("passing")
+
