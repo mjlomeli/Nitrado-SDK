29fd2aa6680d9fd8ce27b94218b5e1a36cc628ce
Krzysztof Biernat <kbiernat@users.noreply.github.com>
2022-10-25 20:45:02 +0200

    Update game_server.py
    Fixed parameter in update_settings
    Please update wiki, https://github.com/mjlomeli/Nitrado-SDK/wiki/GameServer#318-update-settings value should only contain value, not key=value

--- src/nitrado/game_server.py
+++ src/nitrado/game_server.py
@@ -419,7 +419,7 @@ class GameServer:
     def update_settings(self, category: str = None, key: str = None, value: str = None):
         assert GameServer.CLIENT is not None, "A client must be initialized for GameServer"
         path = ['services', self.service_id, 'gameservers', 'settings']
-        params = {'category': category, 'key.txt': key, 'value': value}
+        params = {'category': category, 'key': key, 'value': value}
         return GameServer.CLIENT.post(path=path, params=params)
 
     def boost_history(self, page: int = 0):
