1c546dd0d2fa07951ab5e44087ef3107a932d6aa
mjlomeli <mjlomeli@uci.edu>
2023-03-24 17:36:27 -0700

    fixing install locally scripts

--- scripts/install_locally.sh
+++ scripts/install_locally.sh
@@ -1,24 +1,13 @@
-# This installs the package from the test.pypi
-# Only run this file if you're the owner of the upload at test.pypi
-# Otherwise, to test for changes made from your editor
-# run the 'install_locally.sh' file instead
-
-_package_name="nitrado"
-
+# Run this to simulate installing the package.
+# Once ran, restart the python environment and import arkdriver
 if [[ "$OSTYPE" == "linux-gnu" ]] | [[ "$OSTYPE" == "linux-gnu" ]] | [[ "$OSTYPE" == "darwin" ]]
 then
   # POSIX compatibility layer and Linux environment emulation for Windows
-  printf "y" | python3 -m pip uninstall "$_package_name"
-  python3 -m pip install "$_package_name"
-  _version=$(pip freeze | grep "$_package_name.*")
-  printf "\n\033[32m$_version\033[0m\n"
-elif [[ "$OSTYPE" == "msys" ]]
+  pip3 install -e .
+elif [[ "$OSTYPE" == "msys" ]];
 then
   # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-  printf "y" | python -m pip uninstall "$_package_name"
-  python -m pip install "$_package_name"
-  _version=$(pip freeze | grep "$_package_name.*")
-  printf "\n\033[32m$_version\033[0m\n"
+  pip install -e .
 else
   printf "Haven't created a command for $OSTYPE"
 fi
